c1ba6f81831db1c7e7e20d785f46ae3cf5241a8d
HassanAkbar <hassanakbar2@gmail.com>
2022-03-15 16:45:16 +0500

    adding commit update workflow

---
+++ scripts/update_wikis.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+OLD_VALUE = ARGV[0] # 'https://github.com/ietf-svn-conversion/datatracker'
+NEW_VALUE = ARGV[1] # 'https://github.com/ietf-tools/datatracker'
+COMMIT_AUTHOR = ARGV[2]
+
+def execute_command(command)
+  `#{command}`
+  $CHILD_STATUS
+end
+
+Dir.glob('*.md') do |filename|
+  current_content = File.read(filename)
+  updated_content = current_content.gsub(OLD_VALUE, NEW_VALUE)
+
+  File.open(filename, 'w') do |file|
+    file.puts(updated_content)
+  end
+end
+
+unless execute_command('git add .').success?
+  puts('ERROR at git-add !!!')
+  exit(1)
+end
+
+commit_command = "git commit -m \"Updated wiki links\" --author \"#{COMMIT_AUTHOR}\""
+unless execute_command(commit_command).success?
+  puts('ERROR at git-commit !!!')
+  exit(1)
+end
+
+unless execute_command('git push').success?
+  puts('ERROR at push !!!')
+  exit(1)
+end
